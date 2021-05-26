ff2882f4ed7eac42477117d3296e70fd7763e0da
Uriel Guimarães <urieligara@hotmail.com>
2021-05-26 16:53:26 -0300

    lista-de-usuarios-app

---
+++ src/data/users.js
@@ -0,0 +1,31 @@
+export default [
+  {
+    id: 1,
+    name: 'Miguel Avellar',
+    email: 'miguel@empmail.com',
+    avatarUrl:
+      'https://avatarfiles.alphacoders.com/283/thumb-1920-283695.jpg',
+  },
+  {
+    id: 2,
+    name: 'Letícia Toledo',
+    email: 'leticia@empmail.com',
+    avatarUrl:
+      'https://avatarfiles.alphacoders.com/283/thumb-1920-283683.png',
+  },
+  {
+    id: 3,
+    name: 'Júlia Mendes',
+    email: 'julia@empmail.com',
+    avatarUrl:
+      'https://avatarfiles.alphacoders.com/284/thumb-1920-284902.png',
+  },
+  {
+    id: 4,
+    name: 'Daniel Santos',
+    email: 'daniel@empmail.com',
+    avatarUrl:
+      'https://avatarfiles.alphacoders.com/284/thumb-1920-284774.jpg',
+  },
+];
+
